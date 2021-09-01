2a23f1212a50266be635e33e8e8734f7c197dd7d
hp <hp@DESKTOP-G8MAUED>
2021-09-01 15:29:53 -0600

    Agregar archivos de proyecto.

---
+++ proyectosAPP/proyectosAPP/FontsIcons/IconsFonts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyectosAPP.FontsIcons
+{
+	public static class IconFont
+	{
+		public const string DownOpen = "&#xE800;";
+		public const string UpOpen = "&#xE801;";
+		public const string Scissors = "&#xE802;";
+		public const string Book = "&#xE803;";
+		public const string Cog = "&#xE804;";
+		public const string Home = "&#xE805;";
+		public const string Docs = "&#xF0c5;";
+		public const string Paste = "&#xF0ea;";
+		public const string CalendarPlusO = "&#xF271;";
+		public const string CalendarMinusO = "&#xF272;";
+	}
+}
---
+++ proyectosAPP/proyectosAPP/ViewModels/PruebaClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyectosAPP.ViewModels
+{
+    public class PruebaClass
+    {
+        public string Name { get; set; }
+        public string Dato { get; set; }
+        public string Info { get; set; }
+    }
+    
+    public class Pruebas
+    {
+        public List<PruebaClass> Pruebasa { get; set; } = new List<PruebaClass>();
+        public Pruebas()
+        {
+            Pruebasa.Add(new PruebaClass { Name = "Tab 1", Dato = "presentando informacion", Info = "esto es un tab bindeable"});
+            Pruebasa.Add(new PruebaClass { Name = "Tab 2", Dato = "desarrollando informacion", Info = "esto es un tab bindeable opcion dos" });
+        }
+    }
+}
