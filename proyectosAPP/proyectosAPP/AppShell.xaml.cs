5a57efe4fa1452d5cedf63162fd3bbf47dc46974
hp <hp@DESKTOP-G8MAUED>
2021-09-01 15:29:53 -0600

    Codigo ActualidazoAgregar archivos de proyecto.

---
+++ proyectosAPP/proyectosAPP/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using proyectosAPP.ViewModels;
+using proyectosAPP.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace proyectosAPP
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+           
+        }
+
+    }
+}
